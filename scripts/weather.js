49c0dc38cc4f061f28f37e1d7e2f4abb25aa7215
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-09-21 15:40:40 -0400

    Changes

---
+++ scripts/weather.js
@@ -0,0 +1,25 @@
+const cloud = document.querySelector('.cloudy__icon');
+const partcloud = document.querySelector('.partly__cloudy__icon');
+const snowy = document.querySelector('.snowy__icon');
+const stormy = document.querySelector('.stormy__icon');
+const sunny = document.querySelector('.sunny__icon');
+const sunBackdrop = document.querySelector('.sunny__backdrop');
+const partCloudBackdrop = document.querySelector('.partly__cloudy__backdrop');
+const stormyBackdrop = document.querySelector('.stormy__backdrop');
+const snowyBackdrop = document.querySelector('.snowy__backdrop');
+let location;
+document.addEventListener('DOMContentLoaded', () => { 
+    location = 'Detroit, MI';
+    setWeather = getWeather(location);
+});
+
+
+function getWeather(location){
+
+    
+} 
+
+
+function setWeather(){
+
+}
